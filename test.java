5716f4fec3cf3aecdc9f5bd6b3ef6f4876c229cb
therese bunyi <111711591+therbun@users.noreply.github.com>
2023-09-10 11:04:10 -0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,61 @@
+public class test {
+
+}
+
+
+/* 
+static void gradeofperson(boolean sophomore){
+  int x = 15;
+  int y = 16;
+  if (sophomore == true) {
+    System.out.println(x);
+  }
+
+  else{
+    System.out.println(y);
+  }
+}
+
+public static void main(String[] args){
+  gradeofperson(true);
+}
+static void straykidsfan(boolean fanoranti){
+  if (fanoranti == true) {
+    System.out.println("You are cool");
+  }
+
+  else{
+    System.out.println("You are not so cool");
+  }
+}
+public static void main(String[] args) {
+  straykidsfan(false); // Call the checkAge method and pass along an age of 20
+}
+
+static void myMethod(String titletrack , String group) {
+    System.out.println(titletrack + " is from " + group);
+  }
+
+  public static void main(String[] args) {
+    myMethod("HOT", "SEVENTEEN");
+    myMethod("MIROH", "Stray Kids");
+    myMethod("0x1 LOVESONG", "TOMORROW X TOGETHER");
+  } */
+   /*static String testvar = "yourmom";
+    public static void main(String[] args) {
+      System.out.println(testvar);
+      System.out.println("duh duh duh duh");
+    }*/
+
+  /*public class Main {
+    static void myMethod(String fname) {
+      System.out.println(fname + " Refsnes");
+    }
+  
+    public static void main(String[] args) {
+      myMethod("Liam");
+      myMethod("Jenny");
+      myMethod("Anja");
+    }
+  }*/
+
